267c607113b13a8d270f3e4ce6538451efef0289
Daria <dariykurochkina@mail.ru>
2023-11-20 16:11:48 +0500

    1

--- js/main.js
+++ js/main.js
@@ -4,7 +4,4 @@ const PHOTO_COUNT = 25;
 
 const descriptionPhoto = Array.from({length: PHOTO_COUNT}, createDescriptionPhoto);
 
-//descriptionPhoto();
-
-import {createThumbnails} from './thumbnails.js';
-createThumbnails(descriptionPhoto());
+descriptionPhoto();
