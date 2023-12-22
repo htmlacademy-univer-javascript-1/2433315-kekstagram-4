9eb2ec1e1d2fe7c6c12b16eb476b10f66661fc70
Daria <dariykurochkina@mail.ru>
2023-12-22 21:12:39 +0500

    mod12 task1(2)

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -23,24 +23,3 @@ const renderThumbnails = (pictures) => {
 };
 
 export {renderThumbnails};
-
-/*const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const pictureFragment = document.createDocumentFragment();
-const pictureContainerElement = document.querySelector('.pictures');
-
-const createThumbnails = (descriptionPhotos) => {
-  pictureContainerElement.querySelectorAll('.picture').forEach((thumbnail) => thumbnail.remove());
-  descriptionPhotos.forEach(({url, description, likes, comments, id}) => {
-    const thumbnail = pictureTemplate.cloneNode(true);
-    thumbnail.querySelector('.picture__img').src = url;
-    thumbnail.querySelector('.picture__img').alt = description;
-    thumbnail.querySelector('.picture__likes').textContent = likes;
-    thumbnail.querySelector('.picture__comments').textContent = comments.length;
-    thumbnail.dataset.thumbnailId = id;
-    pictureFragment.appendChild(thumbnail);
-  });
-  pictureContainerElement.appendChild(pictureFragment);
-};
-
-export {createThumbnails};
-*/
